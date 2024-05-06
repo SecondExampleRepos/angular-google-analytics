1e0844bb1fa9d817cd28141153e4139233f9f119
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-06 15:13:52 -0700

    edit_file

--- test/unit/debug-mode.js
+++ test/unit/debug-mode.js
@@ -1,9 +1,11 @@
 /* global afterEach, before, beforeEach, describe, document, expect, inject, it, module, spyOn */
+import { module, inject } from 'angular';
+import { AnalyticsProvider, Analytics } from './analytics-types'; // Assuming types are defined in analytics-types.ts
 'use strict';
 
 describe('universal analytics debug mode', function () {
-  beforeEach(module('angular-google-analytics'));
-  beforeEach(module(function (AnalyticsProvider) {
+  beforeEach(() => module('angular-google-analytics'));
+  beforeEach(() => module((AnalyticsProvider: AnalyticsProvider) => {
     AnalyticsProvider
       .setAccount('UA-XXXXXX-xx')
       .useAnalytics(true)
@@ -11,79 +13,79 @@ describe('universal analytics debug mode', function () {
       .enterTestMode();
   }));
 
-  afterEach(inject(function (Analytics) {
+  afterEach(inject((Analytics: Analytics) => {
     Analytics.log.length = 0; // clear log
   }));
 
   it('should have debug mode disabled by default', function () {
-    inject(function (Analytics) {
+    inject((Analytics: Analytics) => {
       expect(Analytics.configuration.debugMode).toBe(false);
     });
   });
 
   it('should have trace debugging mode disabled by default', function () {
-    inject(function (Analytics) {
+    inject((Analytics: Analytics) => {
       expect(Analytics.configuration.traceDebuggingMode).toBe(false);
     });
   });
 
   describe('without trace debugging mode', function () {
-    beforeEach(module(function (AnalyticsProvider) {
+    beforeEach(() => module((AnalyticsProvider: AnalyticsProvider) => {
       AnalyticsProvider.enterDebugMode();
     }));
 
     it('should have debug mode enabled', function () {
-      inject(function (Analytics) {
+      inject((Analytics: Analytics) => {
         expect(Analytics.configuration.debugMode).toBe(true);
       });
     });
 
     it('should have trace debugging disabled', function () {
-      inject(function (Analytics) {
+      inject((Analytics: Analytics) => {
         expect(Analytics.configuration.traceDebuggingMode).toBe(false);
       });
     });
 
     it('should inject the analytics debug script', function () {
-      inject(function (Analytics) {
+      inject((Analytics: Analytics) => {
         expect(Analytics.log[0]).toEqual(['inject', '//www.google-analytics.com/analytics_debug.js']);
       });
     });
 
     it('should not set the analytics trace debugging variable', function () {
       delete window.ga_debug;
-      inject(function (Analytics, $window) {
+      inject((Analytics: Analytics, $window: Window) => {
         expect($window.ga_debug).toEqual(undefined);
       });
     });
   });
 
   describe('with trace debugging mode', function () {
-    beforeEach(module(function (AnalyticsProvider) {
+    beforeEach(() => module((AnalyticsProvider: AnalyticsProvider) => {
       AnalyticsProvider.enterDebugMode(true);
     }));
 
     it('should have debug mode enabled', function () {
-      inject(function (Analytics) {
+      inject((Analytics: Analytics) => {
         expect(Analytics.configuration.debugMode).toBe(true);
       });
     });
 
     it('should have trace debugging enabled', function () {
-      inject(function (Analytics) {
+      inject((Analytics: Analytics) => {
         expect(Analytics.configuration.traceDebuggingMode).toBe(true);
       });
     });
 
     it('should inject the analytics debug script', function () {
-      inject(function (Analytics) {
+      inject((Analytics: Analytics) => {
         expect(Analytics.log[0]).toEqual(['inject', '//www.google-analytics.com/analytics_debug.js']);
       });
     });
 
     it('should set the analytics trace debugging variable', function () {
       delete window.ga_debug;
-      inject(function (Analytics, $window) {
+      inject((Analytics: Analytics, $window: Window) => {
         expect($window.ga_debug).toEqual({ trace: true });
       });
     });
