f48bc4cf655f9ca954c9c488a7838991e3b7515b
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-06 15:13:47 -0700

    edit_file

--- test/unit/disable-analytics.js
+++ test/unit/disable-analytics.js
@@ -1,44 +1,46 @@
 /* global afterEach, before, beforeEach, describe, document, expect, inject, it, module, spyOn */
+import { module, inject } from 'angular';
+import { AnalyticsProvider, Analytics } from './types'; // Assuming types are defined in types.ts
 'use strict';
 
 describe('disable analytics / user opt-out', function () {
-  beforeEach(module('angular-google-analytics'));
-  beforeEach(module(function (AnalyticsProvider) {
+  beforeEach(() => module('angular-google-analytics'));
+  beforeEach(() => module((AnalyticsProvider: AnalyticsProvider) => {
     AnalyticsProvider
       .setAccount('UA-XXXXXX-xx')
       .logAllCalls(true)
       .enterTestMode();
   }));
 
-  afterEach(inject(function (Analytics) {
+  afterEach(inject((Analytics: Analytics) => {
     Analytics.log.length = 0; // clear log
   }));
 
   describe('with universal analytics', function () {
-    beforeEach(module(function (AnalyticsProvider) {
+    beforeEach(() => module((AnalyticsProvider: AnalyticsProvider) => {
       AnalyticsProvider.useAnalytics(true);
     }));
 
     it('should be enabled by default', function () {
-      inject(function (Analytics) {
+      inject((Analytics: Analytics) => {
         expect(Analytics.configuration.disableAnalytics).toBe(false);
       });
     });
 
     describe('when disabled', function () {
-      beforeEach(module(function (AnalyticsProvider) {
+      beforeEach(() => module((AnalyticsProvider: AnalyticsProvider) => {
         AnalyticsProvider.disableAnalytics(true);
       }));
 
       it('should be disabled', function () {
-        inject(function (Analytics) {
+        inject((Analytics: Analytics) => {
           expect(Analytics.configuration.disableAnalytics).toBe(true);
         });
       });
 
       it('should log an info message about the account being disabled', function () {
-        inject(function (Analytics, $window) {
-          expect(Analytics.log[0]).toEqual([ 'info', 'Analytics disabled: UA-XXXXXX-xx' ]);
+        inject((Analytics: Analytics, $window: Window) => {
+          expect(Analytics.log[0]).toEqual(['info', 'Analytics disabled: UA-XXXXXX-xx']);
           expect($window['ga-disable-UA-XXXXXX-xx']).toBe(true);
         });
       });
@@ -46,30 +48,30 @@ describe('disable analytics / user opt-out', function () {
   });
 
   describe('with classic analytics', function () {
-    beforeEach(module(function (AnalyticsProvider) {
+    beforeEach(() => module((AnalyticsProvider: AnalyticsProvider) => {
       AnalyticsProvider.useAnalytics(false);
     }));
 
     it('should be enabled by default', function () {
-      inject(function (Analytics) {
+      inject((Analytics: Analytics) => {
         expect(Analytics.configuration.disableAnalytics).toBe(false);
       });
     });
 
     describe('when disabled', function () {
-      beforeEach(module(function (AnalyticsProvider) {
+      beforeEach(() => module((AnalyticsProvider: AnalyticsProvider) => {
         AnalyticsProvider.disableAnalytics(true);
       }));
 
       it('should be disabled', function () {
-        inject(function (Analytics) {
+        inject((Analytics: Analytics) => {
           expect(Analytics.configuration.disableAnalytics).toBe(true);
         });
       });
 
       it('should log an info message about the account being disabled', function () {
-        inject(function (Analytics, $window) {
-          expect(Analytics.log[0]).toEqual([ 'info', 'Analytics disabled: UA-XXXXXX-xx' ]);
+        inject((Analytics: Analytics, $window: Window) => {
+          expect(Analytics.log[0]).toEqual(['info', 'Analytics disabled: UA-XXXXXX-xx']);
           expect($window['ga-disable-UA-XXXXXX-xx']).toBe(true);
         });
       });
