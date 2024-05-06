975d1321112396a2e13d14774c34ed39d37730b3
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-06 15:13:56 -0700

    edit_file

--- test/unit/track-exception.js
+++ test/unit/track-exception.js
@@ -1,57 +1,70 @@
 /* global afterEach, before, beforeEach, describe, document, expect, inject, it, module, spyOn */
 'use strict';
 
-describe('universal analytics exception tracking', function () {
-  beforeEach(module('angular-google-analytics'));
-  beforeEach(module(function (AnalyticsProvider) {
-    AnalyticsProvider
-      .setAccount('UA-XXXXXX-xx')
-      .useAnalytics(true)
-      .logAllCalls(true)
-      .enterTestMode();
-  }));
+import { module, inject, afterEach, beforeEach, describe, it } from '@angular/core/testing';
+import { AnalyticsService } from './analytics.service'; // Assuming the service is defined in this path
 
-  afterEach(inject(function (Analytics) {
+interface Tracker {
+  tracker: string;
+  name?: string;
+  trackEvent?: boolean;
+}
+
+describe('universal analytics exception tracking', () => {
+  beforeEach(() => {
+    module('angular-google-analytics');
+    module((AnalyticsProvider: any) => {
+      AnalyticsProvider
+        .setAccount('UA-XXXXXX-xx')
+        .useAnalytics(true)
+        .logAllCalls(true)
+        .enterTestMode();
+    });
+  });
+
+  afterEach(inject((Analytics: AnalyticsService) => {
     Analytics.log.length = 0; // clear log
   }));
 
-  it('should have a trackException method', function () {
-    inject(function (Analytics) {
+  it('should have a trackException method', () => {
+    inject((Analytics: AnalyticsService) => {
       expect(typeof Analytics.trackException === 'function').toBe(true);
     });
   });
 
-  it('should allow for tracking an exception with no parameters provided', function () {
-    inject(function (Analytics) {
+  it('should allow for tracking an exception with no parameters provided', () => {
+    inject((Analytics: AnalyticsService) => {
       Analytics.log.length = 0; // clear log
       Analytics.trackException();
       expect(Analytics.log[0]).toEqual(['send', 'exception', { exDescription: undefined, exFatal: false }]);
     });
   });
 
-  it('should allow for tracking an exception with all parameters provided', function () {
-    inject(function (Analytics) {
+  it('should allow for tracking an exception with all parameters provided', () => {
+    inject((Analytics: AnalyticsService) => {
       Analytics.log.length = 0; // clear log
       Analytics.trackException('Something fatal happened!', true);
       expect(Analytics.log[0]).toEqual(['send', 'exception', { exDescription: 'Something fatal happened!', exFatal: true }]);
     });
   });
 
-  describe('supports tracking for multiple tracking objects', function () {
-    var trackers = [
+  describe('supports tracking for multiple tracking objects', () => {
+    const trackers: Tracker[] = [
       { tracker: 'UA-12345-12', name: 'tracker1', trackEvent: true },
       { tracker: 'UA-12345-34', name: 'tracker2' },
       { tracker: 'UA-12345-45', trackEvent: true }
     ];
 
-    beforeEach(module(function (AnalyticsProvider) {
-      AnalyticsProvider.setAccount(trackers);
-    }));
+    beforeEach(() => {
+      module((AnalyticsProvider: any) => {
+        AnalyticsProvider.setAccount(trackers);
+      });
+    });
 
-    it('should track exceptions for all objects', function () {
-      inject(function ($window) {
+    it('should track exceptions for all objects', () => {
+      inject(($window: any) => {
         spyOn($window, 'ga');
-        inject(function (Analytics) {
+        inject((Analytics: AnalyticsService) => {
           Analytics.trackException('Something fatal happened!', true);
           expect($window.ga).toHaveBeenCalledWith('tracker1.send', 'exception', { exDescription: 'Something fatal happened!', exFatal: true });
           expect($window.ga).toHaveBeenCalledWith('tracker2.send', 'exception', { exDescription: 'Something fatal happened!', exFatal: true });
