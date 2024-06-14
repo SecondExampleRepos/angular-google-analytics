7de33097c10105cb92f51280e330ef1c521eeb94
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-14 20:56:57 +0000

    create_file_with_ai

---
+++ react/src/services/$route.ts
@@ -0,0 +1,32 @@
+// react/src/services/$route.ts
+
+interface Route {
+  pageTrack?: string;
+  templateUrl?: string;
+  doNotTrack?: boolean;
+}
+
+interface Routes {
+  [key: string]: Route;
+}
+
+class RouteService {
+  routes: Routes;
+
+  constructor() {
+    this.routes = {
+      someroute: { pageTrack: '/some' },
+      otherroute: {}
+    };
+  }
+
+  getCurrentRoute(path: string): Route | undefined {
+    return this.routes[path];
+  }
+
+  addRoute(path: string, route: Route): void {
+    this.routes[path] = route;
+  }
+}
+
+export default RouteService;
